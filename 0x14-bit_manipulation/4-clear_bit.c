f4af394fcd38b233f9f975ba297410fa547dc04f
AmaanDV <dvamaan@gmail.com>
2023-04-12 04:13:46 -0700

    updated

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,12 +4,18 @@
  * @Amaanthis code would set the value of a given bit to 0
  * And return a value 
  */
-int clear bit(unsigned long int *n, unsigned int index)
+int clear bit (unsigned long int *n, unsigned int index)
 {
+	unsigned int m;
+
 	if (index > 63)
 		return (-1);
 
-	*n = (~(1UL << index) & *n);
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
 	return (1);
 }
 
